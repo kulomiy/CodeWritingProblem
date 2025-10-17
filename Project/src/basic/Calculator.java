f6df470431a417cba16af2592de56a5a2b6a7841
kulomiy <nabilapebmahrani@gmail.com>
2025-10-17 21:24:26 +0700

    commit

---
+++ Project/src/basic/Calculator.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Calculator {   
+
+    // method untuk menjumlahkan dua bilangan bulat
+    public int add (int a, int b) {
+        // mengembalikan hasil perjumlahan a dan b
+        return a + b;
+    }
+}
